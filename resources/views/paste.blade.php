e7fb2eec227350b3514a6c07b19cdcd83e182273
DronT1 <vowa.matros@yandex.ru>
2022-08-09 14:49:39 +0700

    Доделаны страницы "мои пасты", "паста", реализовано получение моих последних паст и их отображение

--- resources/views/paste.blade.php
+++ resources/views/paste.blade.php
@@ -1,7 +1,18 @@
 <x-layouts.app>
     <div>
         @if ($errorAccess) <h3>Ссылка недоступна!</h3>
-        @else <h3>Паста:</h3>
+        @else
+            <div>
+                <h2>{{$pasteData['title']}}</h2>
+                <p>Срок пасты истекает: {{$pasteData['expiration']}}</p>
+                <div style="max-width: 800px;">
+                    <pre>
+                        <code class="{{$pasteData['syntax']}}">
+                            {{$pasteData['description']}}
+                        </code>
+                    </pre>
+                </div>
+            </div>
         @endif
     </div>
 </x-layouts.app>
